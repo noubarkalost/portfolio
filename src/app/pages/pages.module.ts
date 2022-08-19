c20361b38b225c74b1c98d51f96cb8535b88339e
noubar <noubar.kaloust@gmail.com>
2022-08-19 13:01:16 +0400

    adding card

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,7 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { SharedModule } from "../shared/shared.module";
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     PagesRoutingModule
   ]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HeaderComponent} from "./header/header.component";
 import {RouterModule} from "@angular/router";
+
+import {HeaderComponent} from "./header/header.component";
 import { FooterComponent } from './footer/footer.component';
 import { ButtonComponent } from './button/button.component';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, ButtonComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
+    CardComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    CardComponent
+  ]
 })
 export class SharedModule { }
